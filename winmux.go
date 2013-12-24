e3f3d664cffe686475d4332de504610689fc5d34
Robert Kroeger <rjkroege@chromium.org>
2013-12-24 12:40:35 -0700

    Added some source

---
+++ winmux.go
@@ -0,0 +1,19 @@
+/*
+
+    A version of win written in Go. That does terminal multiplexing.
+
+*/
+
+package main
+
+import (
+	"fmt"
+//	"log"
+//	"code.google.com/p/goplan9/draw"
+//	"image"
+)
+
+func main() {
+	fmt.Print("hello from winmux\n");
+	fmt.Print("bye\n")
+}
